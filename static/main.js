79a3db292697c9e214b8c96493a2d8927fc510b5
Ryan Maugin <ryanmauginn@gmail.com>
2017-08-23 14:50:07 +0100

    Frontend added

---
+++ static/main.js
@@ -0,0 +1,19 @@
+//
+//  Natter
+//  main.js
+//
+//  Created on 23/08/17
+//
+
+$(document).ready(function () {
+
+	// Handle the carousel behaviour
+	$('.slick-carousel').slick({
+		dots: true,
+		autoplay: true,
+		infinite: true,
+  		autoplaySpeed: 5000
+	});
+
+});
+
