37a89598b0365925897cb0fc2c0b08fe4536c036
Ryan Maugin <ryanmauginn@gmail.com>
2017-08-24 15:15:53 +0100

    Added video view ui

--- static/main.js
+++ static/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 
 	// Hide the category view initially until chosen to view
 	$('#category-view').hide();
+	$('#video-view').hide();
 
 	// Handle the carousel behaviour
 	$('.slick-carousel').slick({
@@ -18,16 +19,28 @@ $(document).ready(function () {
 	});
 
 	// This will handle the showing of the category view
-	$('#show-category').click(function () {
+	$('.show-category').click(function () {
+		console.log('showing category')
+		$('#video-view').fadeOut(700);
 		$('#main-view').fadeOut(700);
 		$('#category-view').fadeIn(1400);
 	});
 
 	// This will handle the showing of the main view
-	$('#show-main').click(function () {
+	$('.show-main').click(function () {
+		console.log('showing main')
+		$('#video-view').fadeOut(700);
 		$('#category-view').fadeOut(700);
 		$('#main-view').fadeIn(1400);
-	})
+	});
+
+	// This will handle showing video view
+	$('.show-video').click(function () {
+		console.log('showing video')
+		$('#category-view').fadeOut(700);
+		$('#main-view').fadeOut(700);
+		$('#video-view').fadeIn(1400);
+	});
 
 });
 
