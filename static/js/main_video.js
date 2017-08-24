8f5a6e5528eb1043d63dc3881c0272a3f77c7c72
Tom <tkelross@gmail.com>
2017-08-24 21:22:04 +0100

    On our way to card nirvana

--- static/js/main_video.js
+++ static/js/main_video.js
@@ -3,38 +3,83 @@
  */
 
 
-function tether_card() {
+//Article functions
+function get_article(id) {
+    return allArticles[id]
+}
+
+//Card Related Functions
+function show_card() {
     //Creates a new tether for the card
     vid_card = $('#vid-card')
     vid = $('#my-video')
-    new Tether({
-        element: vid_card,
-        target: vid,
-        attachment: 'top center',
-        targetAttachment: 'top center',
-        // targetModifier: 'hidden'
-        offset: '-15px 0px',
-        enabled: true
-    })
+    vid_card.show()
+    setTimeout(function () {
+        new Tether({
+            element: vid_card,
+            target: vid,
+            attachment: 'top center',
+            targetAttachment: 'top center',
+            // targetModifier: 'hidden'
+            offset: '-15px 0px',
+            enabled: true
+        })
+    }, 10);
 }
 
+function show_pause() {
+    pause_button = $('#vid-pause-icon')
+    vid = $('#my-video')
+    vid_card.show()
+    setTimeout(function () {
+        new Tether({
+            element: pause_button,
+            target: vid,
+            attachment: 'top center',
+            targetAttachment: 'top left',
+            // targetModifier: 'hidden'
+            offset: '0px 0px',
+            enabled: true
+        })
+    }, 10);
+}
 
+function hide_card(){
+    vid_card = $('#vid-card')
+    vid_card.hide()
+}
 
-function play_video(url) {
-    vid_id = 2
+function hide_pause(){
+    pause_button = $('#vid-pause-icon')
+    pause_button.hide()
+}
+
+$('#vid-pause-icon').click(function (e) {
     video_player = videojs('my-video');
-    video_player.src(url);
+    video_player.pause()
+});
+
+
+
+
+function play_video(article) {
+    video_player = videojs('my-video');
+    video_player.src(article.videoSRC);
     // debugger;
     video_player.ready(function () {
         $('#my-video').show()
-        setTimeout(function(){ video_player.play(); }, 300);
-        reset_video_activations(vid_id)
-        tether_card()
+        show_pause()
+        register_article_cards(article)
+        setTimeout(function () {
+            video_player.play();
+        }, 300);
+        reset_video_card_activations(article)
 
     });
 
 
     video_player.on('ended', function () {
+        hide_pause()
         $('#my-video').hide()
         show_article("hi")
         vid_card.hide()
@@ -50,58 +95,83 @@ function play_video(url) {
 
     video_player.on('timeupdate', function () {
         // vid_card.text(this.currentTime());
-        check_time_register(vid_id, this.currentTime())
+        check_card_register(article, this.currentTime())
     })
 }
 
-videos_time_register = {}
+article_cards_register = {}
 
 // Calls a function at a particular time for a particular video
-function register_time_callback(v_id, vtime, callback) {
-    if (videos_time_register[v_id]) {
-        videos_time_register[v_id].push({
+function register_card_callback(v_id, card, callback) {
+    if (article_cards_register[v_id]) {
+        vtime = card.time
+        article_cards_register[v_id].push({
             video_id: v_id,
+            card : card,
             video_time: vtime,
             callback: callback,
             activated: false
         })
     } else {
-        videos_time_register[v_id] = []
-        videos_time_register[v_id].push({
+        article_cards_register[v_id] = []
+        vtime = card.time
+        article_cards_register[v_id].push({
             video_id: v_id,
+            card : card,
             video_time: vtime,
             callback: callback,
-            activated: false,
+            activated: false
         })
     }
 }
 
 
-
-function check_time_register(video_id, currentTime) {
-    video_register = videos_time_register[video_id]
+function check_card_register(article, currentTime) {
+    video_id = article.id
+    video_register = article_cards_register[video_id]
 
     for (var index = 0; index < video_register.length; index++) {
         var item = video_register[index];
         if (item.activated == false) {
             if (currentTime > item.video_time) {
-                item.callback(video_id, currentTime)
-                videos_time_register[video_id][index].activated = true
+                item.callback(video_id,item.card, currentTime)
+                article_cards_register[video_id][index].activated = true
             }
         }
     }
 }
 
-function reset_video_activations(video_id) {
-    video_register = videos_time_register[video_id]
+function reset_video_card_activations(video_id) {
+    video_register = article_cards_register[video_id]
     if (video_register) {
         for (var index = 0; index < video_register.length; index++) {
-            videos_time_register[video_id][index].activated = false
+            article_cards_register[video_id][index].activated = false
         }
     }
 }
 
+function register_article_cards(article) {
+    a_cards = article.cards
+    article_id = article.id
+    for (var index = 0; index < a_cards.length; index++) {
+        var card = a_cards[index];
+        register_card_callback(article_id,card,show_card)
+        }
+    }
 
+function handle_card(id, card, current_time) {
+    debugger;
+    show_card(card)
+    setTimeout(function () {
+        vid_card.show();
+        ;
+    }, 300);
+}
+
+
+function show_article(content) {
+    $('#article-modal').modal()
+}
 
 //this class should be given to anything that is meant to start a video
 $('.video-starter').click(function (e) {
@@ -113,9 +183,8 @@ $('.video-starter').click(function (e) {
     vid_url = article.videoSRC;
     // vid_url = "https://s3-eu-west-1.amazonaws.com/content.natter-london.com/sophie+video+1.mp4"
     // vid-url = e.attr('url')
-    play_video(vid_url)
+    play_video(article)
 });
-
 
 
 $(document).ready(function () {
@@ -128,7 +197,6 @@ $(document).ready(function () {
     vid.hide()
 
     vid_card = $('#vid-card')
-    tether_card()
 
 
     vid_card.click(function (e) {
@@ -145,28 +213,9 @@ $(document).ready(function () {
         }
     });
 
-    function show_article(content) {
-        $('#article-modal').modal()
-    }
-
     function update_card_text(params) {
 
     }
-
-
-    function show_card(id, time) {
-        // in future get card text from id
-        // etc
-
-
-        // card_details = get_card_details()
-        vid_card.show()
-        tether_card()
-    }
-
-
-    register_time_callback(2, 1, show_card)
-
 
 
     //play the intro video
@@ -198,5 +247,5 @@ $(document).ready(function () {
 
 // video_player.on('timeupdate', function () {
 // 	// vid_card.text(this.currentTime());
-// 	check_time_register(vid_id, this.currentTime())
+// 	check_card_register(vid_id, this.currentTime())
 // })
--- static/json/templateArticles.js
+++ static/json/templateArticles.js
@@ -1,4 +1,42 @@
 var allArticles = [
-    { "title":"Natter", "genre":"Testing", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":{src: 'https://content.natter-london.com/job2/sophie1.mpd', type: 'application/dash+xml'}, "shortDesc":"BLAAAAAGH", "longDesc":"BLAAAAAAAAAAAAAAAGH", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this BLAGH","0","0"]},
-    { "title":"Natter", "genre":"Testing", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"BLAAAAAGH", "longDesc":"BLAAAAAAAAAAAAAAAGH", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this BLAGH","0","0"]}
+    {
+        "id": 0,
+        "title": "Natter",
+        "genre": "Testing",
+        "date": "22/08/2017",
+        "imgSRC": "http://via.placeholder.com/300x200",
+        "videoSRC": {src: 'https://content.natter-london.com/job2/sophie1.mpd', type: 'application/dash+xml'},
+        "shortDesc": "BLAAAAAGH", //todo: refactor this to headline and tagline
+        "longDesc": "BLAAAAAAAAAAAAAAAGH",
+        "cards": [{
+            "time": 1,
+            "tag": "News Uk Office",
+            "text": "This is office of News UK",
+            "links": []
+        }],
+        "placeTimes": ["0:00", "0:02", "0:05"],
+        "placeDesc": ["Visit this BLAGH", "0", "0"],
+        "articleTitle" : "Blah blah",
+        "articleContent": "Help i'm stuck in a chineese cookie factory"
+    },
+    {
+        "id": 1,
+        "title": "Natter",
+        "genre": "Testing",
+        "date": "22/08/2017",
+        "imgSRC": "http://via.placeholder.com/300x200",
+        "videoSRC": "https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4",
+        "shortDesc": "Introduction",
+        "longDesc": "Watch that amazing introduction again",
+        "placeTimes": ["0:00", "0:02", "0:05"],
+        "placeDesc": ["Visit this BLAGH", "0", "0"],
+        "cards": [{
+            "time": 2,
+            "tag": "Intro",
+            "text": "This is our offical intro",
+            "links": []
+        }],
+        "articleTitle" : "Testing",
+        "articleContent": "Testing"
+    }
 ]
