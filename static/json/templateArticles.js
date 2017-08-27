e2af5f716677f4c2678b1e8ea2baf6f0dbae00a7
Tom <tkelross@gmail.com>
2017-08-27 10:18:50 +0100

    Final Tweaks that work and were used to present

--- static/json/templateArticles.js
+++ static/json/templateArticles.js
@@ -1,9 +1,104 @@
 var allArticles = [
-    { "title":"Natter", "genre":"Testing", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":{src: 'https://content.natter-london.com/job2/sophie1.mpd', type: 'application/dash+xml'}, "shortDesc":"BLAAAAAGH", "longDesc":"BLAAAAAAAAAAAAAAAGH", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this BLAGH","0","0"]},
-    { "title":"Natter", "genre":"Testing", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"BLAAAAAGH", "longDesc":"BLAAAAAAAAAAAAAAAGH", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this BLAGH","0","0"]},
-    { "title":"Natter", "genre":"Music", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"music description", "longDesc":"Musicccc", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this music store","0","0"]},
-    { "title":"Natter", "genre":"Science", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"science description", "longDesc":"sciencccce", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this science museum","0","0"]},
-    { "title":"Natter", "genre":"Fashion", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"Fashion description", "longDesc":"Fashionnnn", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this boutique","0","0"]},
-    { "title":"Natter", "genre":"Food", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"Food description", "longDesc":"Fooood", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this place","0","0"]},
-    { "title":"Natter", "genre":"Places", "date":"22/08/2017", "imgSRC":"http://via.placeholder.com/300x200", "videoSRC":"https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4", "shortDesc":"Places description", "longDesc":"Placesss", "placeTimes":["0:00","0:02","0:05"], "placeDesc":["Visit this place","0","0"]}
+    {
+        "id": 0,
+        "title": "A Natter with: Brendan Owens at The Royal Observatory Greenwich",
+        "genre": "Science",
+        "date": "22/08/2017",
+        "imgSRC": "https://content.natter-london.com/observatory/thumbnail.png",
+        // "videoSRC": {src: 'https://content.natter-london.com/observatory/playlist2.mpd', type: 'application/dash+xml'},
+        "shortDesc": "London's 'Starman'  at the home of time and space", //todo: refactor this to headline and tagline
+        "videoSRC": "https://s3-eu-west-1.amazonaws.com/content.natter-london.com/Royal+observatory_1.mp4",
+        "longDesc": "London's 'Starman'  at the home of time and space",
+        "cards": [{
+            "time": 1,
+            "tag": "News Uk Office",
+            "text": "This is office of News UK",
+            "links": []
+        },{
+            "time": 1.5,
+            "tag": "Office of the News",
+            "text": "This is office of News UK",
+            "links": []
+        },
+            {
+                "time": 7,
+                "tag": "Keep watching",
+                "text": "This is office of News UK",
+                "links": []
+            },
+            {
+                "time": 10,
+                "tag": "Yes Yes",
+                "text": "This is office of News UK",
+                "links": []
+            }
+        ],
+        "articleTitle" : "A Natter with Brendan Owens at The Royal Observatory Greenwich",
+        "articleContent": "Hidden in the heart of Greenwich Park lies the Royal Observatory Greenwich, home to the world’s 7th largest lens telescope, London’s only planetarium and Brendan Owens; a David Bowie fan, astronomer and public engagement manager at the Observatory. Brendan certainly believes this 342 year old site has some hidden gems to offer visitors that they wouldn’t experience when visiting other attractions. They have the chance to not only stand where some of the most famous figures in astronomy have stood but also to simultaneously be in the east and west of the world whilst taking in the most picturesque view in London. Founded initially to overcome sailor’s problems with navigating, the Royal Observatory created star maps to help them know how far east or west they were. This was not just a problem for local sailors, it was a problem for sailors all over the world. Therefore the position at which the telescope used to map stars from became known as the Greenwich Meridian Line, the 0 degrees latitude point on modern maps, creating a unified system of times zones as they travelled across the world. The observatory didn’t stop at star mapping, and other departments such as astrophotography and solar physics were formed over the centuries, culminating in work today being done on sun spots - areas much cooler than the sun’s core where storms are occurring at its surface. Brendan is currently involved with a project called the telescope appeal, looking to fundraise money for a new set of instruments to be used at the Altazimuth building. Dating back to 1899, the building is under used in terms of research, and the appeal hopes to transform it into an urban observatory, bringing real observing and active research to the observatory, putting it at the heart of future discovery. Although, there are still ways of reaching outer space currently at the observatory in the shape of the great equatorial telescope, situated in the green ‘onion’ dome. Sent to a safer place during world war two, the telescope has been used to view the moon, the andromeda galaxy and can be used by visitors to view Venus during the daytime providing a truly out of this world experience. The observatory holds a close place in Brendan’s heart having visited as a child with his uncle, poking his head through the gates, gazing at the incredible architecture and at the place where great discoveries were made. The Royal Observatory was home to all Astronomer Royals until 1972, including the likes of Edmond Halley who computed the orbit of Halley’s Comet and James Bradley who obtained proof that the earth orbits the sun; the telescope which he used to do so is still housed at the observatory. These discoveries were all made in amongst the beautiful architecture created by Sir Christopher Wren, from the Octagon room to Flamsteed house, a reason alone to warrant a visit to the Royal Observatory in Greenwich. Situated in a UNESCO world heritage site, the Royal Observatory is no stranger to famous people with Peter the Great visiting to have a drink with the astronomer royal at the time, causing havoc in the process. Whether it be Russian Tsars like Peter the Great, or space enthusiasts like Brendan, the Royal Observatory in Greenwich can offer a multitude of people an incredibly unique and epic experience in the city of dreams, London. "
+    },
+    {
+        "id": 1,
+        "title": "A Natter with: Laviai Nielsen",
+        "genre": "Science",
+        "date": "22/08/2017",
+        "imgSRC": "https://s3-eu-west-1.amazonaws.com/content.natter-london.com/lavai_thumbnail.jpg",
+        // "videoSRC": {src: 'https://content.natter-london.com/observatory/playlist2.mpd', type: 'application/dash+xml'},
+        "shortDesc": "From kit carrier to World silver medalist- a guide to East London's Olympic legacy from one of the generation it inspired ", //todo: refactor this to headline and tagline
+        "videoSRC": "https://s3-eu-west-1.amazonaws.com/content.natter-london.com/lavai+nelson.mp4",
+        "longDesc": "London's 'Starman'  at the home of time and space",
+        "cards": [{
+            "time": 1,
+            "tag": "News Uk Office",
+            "text": "This is office of News UK",
+            "links": []
+        }, {
+            "time": 1.5,
+            "tag": "Office of the News",
+            "text": "This is office of News UK",
+            "links": []
+        },
+            {
+                "time": 7,
+                "tag": "Keep watching",
+                "text": "This is office of News UK",
+                "links": []
+            },
+            {
+                "time": 10,
+                "tag": "Yes Yes",
+                "text": "This is office of News UK",
+                "links": []
+            }
+        ],
+        "articleTitle": "A Natter with Brendan Owens at The Royal Observatory Greenwich",
+        "articleContent": "In the five years since London 2012, 'Super Saturday’ has become British sport’s 21st Century 'I was there’ moment. Just as five times the capacity of the old Wembley Stadium will tell you they saw Bobby Moore lift the Jules Rimet trophy in 1966, the number who claim to have witnessed the triple crowning of messers Ennis, Farah and Rutherford on that summer’s night in Stratford will only multiply in the years to come. <br><br>Of the lucky 80,000 who did, few could have had a better view than Laviai Nielsen, then a 16-year-old schoolgirl, tasked with carrying kit for one of this drama’s main protagonists, the Games’ poster girl, Jessica Ennis. <br><br>'I remember coming out from underneath the stadium and there were crowds of people leaning over, shouting her name, screaming her name,’ Nielsen recalls. 'But it was like she couldn’t hear any of it. She was so focused and so determined, and I thought '“that’s what I need”.’<br><br>Five years on, Nielsen stands as one of the great realisations of that Games’ promise to 'inspire a generation’.  Though the seeds were sown way back in 2005, when her primary school became a specialist in sport following the successful Olympic bid, the Nielsen flower truly blossomed this summer as, back in the stadium where her idol won gold, she ran a sublime second leg to help Britain’s 4x400m quartet to World Championship silver. <br><br>Back in July, however, London and the prospect of hometown glory could not have seemed further away. A nightmare run at the British team trials in Birmingham saw Nielsen fail to even make the final of her individual event. <br><br>'At that point I didn’t think London was going to happen for me. I just thought “that’s it, my season’s over.” So for them to select me in the 4x400 [for the World Championships] was a massive leap of faith.’<br><br>It was faith that Nielsen quickly repaid, running the team’s fastest leg in the heat, a 50.5 second lap, to prove her true form. In the final she took the baton from sixth to second on her leg, unfazed as Jamaica, one of Britain’s biggest medal rivals, pulled up injured outside her.  <br><br>'I think we’d done three relays this year before Worlds where we came fourth,’ she sighs. 'I just thought “we cannot come fourth in front of a home crowd”. I was petrified that maybe we weren’t going to get a medal but thankfully we did.’<br><br>Nielsen’s love for her city remains unavoidable. In a previous interview when asked where she sees herself in 10 years’ time, she said not “world champion” or “Olympic medallist”, but simply “in London”. <br><br>While many young British athletes head to the US, or one of the UK’s leading sports universities to marry their studies with high class facilities and training groups, Nielsen had a late change of heart, choosing Kings College London over Loughborough. There she reads Geography, much of her study revolving around the regeneration of East London that was, like her running career, sparked by the Games of 2012. <br><br>'The whole image of the area changed. It became a lot safer, East London was known as quite a rough area. That just made me realise how significant sport is.’ <br><br>Yet it is interesting that Nielsen draws distinction between that London, her London, and 'London London’. Her first memory of the city proper is of the bright lights of Piccadilly Circus, and if she had only one day in the City she’d flock to the West End to fulfil a childhood dream of watching The Lion King. <br><br>Her favourite place to run though? That’s easy. 'Through the Olympic Park. It just has so many memories. It’s the perfect setting for that.'"},
+    {  "id": 2,
+        "title": "Natter",
+        "genre": "Testing",
+        "date": "22/08/2017",
+        "imgSRC": "http://via.placeholder.com/300x200",
+        "videoSRC": "https://s3.eu-west-2.amazonaws.com/natter-london.com/Natter+warm+up.mp4",
+        "shortDesc": "Introduction",
+        "longDesc": "Watch that amazing introduction again",
+        "placeTimes": ["0:00", "0:02", "0:05"],
+        "placeDesc": ["Visit this BLAGH", "0", "0"],
+        "cards": [{
+            "time": 0.4,
+            "tag": "Intro",
+            "text": "This is our offical intro",
+            "links": []
+        },
+            {
+                "time": 1,
+                "tag": "Quick change",
+                "text": "Ayy",
+                "links": []
+            }
+            ]
+        ,
+
+
+        "articleTitle" : "Testing",
+        "articleContent": "Testing"
+    }
 ]
